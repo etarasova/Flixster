aed86f075c1aaf9eed0b4cf953437071298e6901
etarasova <taraasik@gmail.com>
2020-10-03 02:03:32 +0300

    Flixster version 2

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,18 +3,25 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+@Parcel
 public class Movie {
-    private int id;
+    private int movieId;
     private String posterPath;
     private String originalTitle;
     private double voteAverage;
     private String overview;
     private String releaseDate;
+    private double rating;
+
+    // empty constructor needed by the Parceler library
+    public Movie() {
+    }
 
     public static List<Movie> fromJsonArray(JSONArray jsonArray) throws JSONException {
         List<Movie> movies = new ArrayList<>(jsonArray.length());
@@ -27,21 +34,23 @@ public class Movie {
 
     public static Movie fromJson(JSONObject jsonObject) throws JSONException {
         Movie movie = new Movie();
-        movie.setId(jsonObject.getInt("id"));
+        movie.setMovieId(jsonObject.getInt("id"));
         movie.setOriginalTitle(jsonObject.getString("original_title"));
         movie.setPosterPath(jsonObject.getString("poster_path"));
         movie.setVoteAverage(jsonObject.getDouble("vote_average"));
         movie.setOverview(jsonObject.getString("overview"));
         movie.setReleaseDate(jsonObject.getString("release_date"));
+        movie.setRating(jsonObject.getDouble("vote_average"));
+
         return movie;
     }
 
-    public int getId() {
-        return id;
+    public int getMovieId() {
+        return movieId;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
     }
 
     public String getPosterPath() {
@@ -88,10 +97,18 @@ public class Movie {
         return String.format("https://image.tmdb.org/t/p/w342/%s",getPosterPath());
     }
 
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
     @Override
     public String toString() {
         return "Movie{" +
-                "id=" + id +
+                "id=" + movieId +
                 ", posterPath='" + posterPath + '\'' +
                 ", originalTitle='" + originalTitle + '\'' +
                 ", voteAverage=" + voteAverage +
@@ -105,11 +122,11 @@ public class Movie {
         if (this == o) return true;
         if (!(o instanceof Movie)) return false;
         Movie movie = (Movie) o;
-        return getId() == movie.getId();
+        return getMovieId() == movie.getMovieId();
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId());
+        return Objects.hash(getMovieId());
     }
 }
